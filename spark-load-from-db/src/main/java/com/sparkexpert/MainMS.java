67b3e210d4bc6c48e3ca5c3d8b7a2ed8f527d4e7
Tom Zeng <tom.z.zeng@gmail.com>
2015-11-18 13:33:59 -0800

    Correct typos

--- spark-load-from-db/src/main/java/com/sparkexpert/MainMS.java
+++ spark-load-from-db/src/main/java/com/sparkexpert/MainMS.java
@@ -32,17 +32,17 @@ public class MainMS implements Serializable {
     private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(Main.class);
 
     private static final String SQLSERVER_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static final String SQLSERVER_USERNAME = System.getenv("SQL_USERNAME");
-    private static final String SQLSERVER_PWD = System.getenv("SQL_PWD");
-    private static final String SQLSERVER_HOST_PORT = System.getenv("SQL_HOST_PORT");
-    private static final String SQLSERVER_DB = System.getenv("SQL_DB");
-    private static final String SQLSERVER_TABLE = System.getenv("SQL_TABLE");
+    private static final String SQLSERVER_USERNAME = System.getenv("SQLSERVER_USERNAME");
+    private static final String SQLSERVER_PWD = System.getenv("SQLSERVER_PWD");
+    private static final String SQLSERVER_HOST_PORT = System.getenv("SQLSERVER_HOST_PORT");
+    private static final String SQLSERVER_DB = System.getenv("SQLSERVER_DB");
+    private static final String SQLSERVER_TABLE = System.getenv("SQLSERVER_TABLE");
     private static final String SQLSERVER_CONNECTION_URL =
             "jdbc:sqlserver://" + SQLSERVER_HOST_PORT + ";database=" + SQLSERVER_DB + ";user=" + SQLSERVER_USERNAME + ";password=" + SQLSERVER_PWD + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
     
     private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
     private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
-    private static final String AWS_S3_BUCKET = System.getenv("S3_BUCKET");
+    private static final String AWS_S3_BUCKET = System.getenv("AWS_S3_BUCKET");
 
     private static final JavaSparkContext sc =
             new JavaSparkContext(new SparkConf().setAppName("SparkJdbcDs").setMaster("local[*]"));
@@ -68,7 +68,7 @@ public class MainMS implements Serializable {
 
         // Save dataframe query to S3
         jdbcDF.select("*").write().format("com.databricks.spark.csv").
-          save("s3n://" + AWS_ACCESS_KEY_ID + ":" + AWS_ACCESS_KEY_ID + "@" + AWS_S3_BUCKET);
+          save("s3n://" + AWS_ACCESS_KEY_ID + ":" + AWS_SECRET_ACCESS_KEY + "@" + AWS_S3_BUCKET);
         
         LOGGER.info("Data saved to s3 bucket: "+AWS_S3_BUCKET);
     }
